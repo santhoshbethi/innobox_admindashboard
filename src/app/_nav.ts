c6edf9a77f6424354dcc732719003391b28c7e6b
SanthoshKumarBethi <santhosh.bethi@wanbuffer.com>
2021-05-02 20:05:45 +0530

    Starting of project

---
+++ src/app/_nav.ts
@@ -0,0 +1,49 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+ 
+ 
+  {
+    name: 'Menu',
+    url: '/menu',
+    icon: 'icon-drop'
+  },
+  {
+    name: 'Careers',
+    url: '/careers',
+    icon: 'icon-pencil'
+  },
+ 
+  {
+    name: 'Services',
+    url: '/services',
+    icon: 'icon-cursor',
+    
+  },
+
+  {
+    name: 'Home ',
+    url: '/home',
+    icon: 'icon-pie-chart'
+  },
+  {
+    name: 'Recent Works',
+    url: '/recentworks',
+    icon: 'icon-pie-chart'
+  },
+    {
+    name: 'Contact Us',
+    url: '/contactus',
+    icon: 'icon-pie-chart'
+  },
+  {
+    name: 'Inbox',
+    url: '/inbox',
+    icon: 'icon-pie-chart'
+  },
+   {
+    divider: true
+  },
+  
+  
+];
