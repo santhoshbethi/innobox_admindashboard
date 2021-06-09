95b19a547e0861e1d19a4877ec4a10b4c17aa187
SanthoshKumarBethi <santhosh.bethi@wanbuffer.com>
2021-06-09 17:39:26 +0530

    add menu updated

--- src/app/views/menu/add-menu/add-menu.component.ts
+++ src/app/views/menu/add-menu/add-menu.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { ApiService } from '../../../services/api.service';
 
+import { Router } from "@angular/router";
+
+
 
 interface Pokemon {
   value: string;
@@ -20,22 +23,25 @@ interface PokemonGroup {
   templateUrl: './add-menu.component.html',
   styleUrls: ['./add-menu.component.scss']
 })
-export class AddMenuComponent implements OnInit {
-
- 
-
-  constructor(
-    public api: ApiService,
-
-  ) {}
-
-  ngOnInit(): void {
-
+export class AddMenuComponent{
+  rows: any;
+  columns: any;
+  constructor(public api: ApiService, private router: Router) {}
+  ngOnInit() {
+    this.getMenu();
+    
+  }
+  getMenu() {
     this.api.getMenu().subscribe((response) => {
       let res = JSON.parse(JSON.stringify(response));
-    });  
+      
+      if (res.message) {
+        this.rows = res.message;
+               
+      } else {
+        console.log("errror", res);
+      }
+    });
   }
-
-  
 
 }
